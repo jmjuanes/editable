dd69bc619a8beac2a742990f6ec736471697ffcb
Josemi <josemijuanes@gmail.com>
2023-08-01 18:55:57 +0200

    Fix executing async scripts and add console wrapper

--- runner.js
+++ runner.js
@@ -1,14 +1,18 @@
-import {RESULT_TYPES} from "./constants.js";
+import {RESULT_TYPES, CONSOLE_LEVELS} from "./constants.js";
 
-const AsyncFunction = Object.getPrototypeOf(async () => {}).constructor;
+const AsyncFunction = Object.getPrototypeOf(async function (){}).constructor;
 
 // Global context for executing code blocks
 let context = {};
 
+// Wrap command for using 'await'
+const wrapCommand = command => {
+    return `return (async () => {${command}})();`;
+};
+
 // Create a new kori instance object
 const createKoriInstance = () => {
     const koriInstance = {
-        _logs: [],
         _parent: document.createElement("div"),
         _renderedHtml: false,
         delay: time => {
@@ -20,21 +24,43 @@ const createKoriInstance = () => {
                 koriInstance._renderedHtml = true;
             }
         },
-        test: value => value + 5,
     };
     return koriInstance;
+};
+
+// Create a new console instance
+const createConsoleInstance = () => {
+    const consoleInstance = {
+        _logs: [],
+        clear: () => {
+            consoleInstance._logs = [];
+        },
+    };
+    // Register console log levels
+    Object.values(CONSOLE_LEVELS).forEach(level => {
+        // TODO: we would need to fully support console[LEVEL] arguments
+        consoleInstance[level] = message => {
+            consoleInstance._logs.push({
+                level: level,
+                message: message,
+                time: Date.now(),
+            });
+        };
+    });
+    return consoleInstance;
 };
 
 // Execute the provide command
 export const execute = async command => {
     const kori = createKoriInstance();
+    const consoleInstance = createConsoleInstance();
     const result = {
         type: RESULT_TYPES.VALUE,
-        logs: kori._logs,
+        logs: consoleInstance._logs,
     };
     try {
-        const fn = new AsyncFunction("kori", command);
-        result.value = await fn.call(context, kori);
+        const fn = new AsyncFunction("kori", "console", wrapCommand(command));
+        result.value = await fn.call(context, kori, consoleInstance);
         // Check if we have rendered HTML content
         if (kori._renderedHtml) {
             result.type = RESULT_TYPES.HTML;
@@ -44,6 +70,7 @@ export const execute = async command => {
     catch(error) {
         // Save error message in result and set as error type
         result.type = RESULT_TYPES.ERROR;
+        result.errorType = error.name;
         result.errorMessage = error.message;
     }
     // Return result object
