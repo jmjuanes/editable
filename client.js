8ee33d86f7ae7a3325f7755fe15518a1c3a8a424
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-08-21 08:59:24 +0000

    Allow to get a list of all local notebooks

--- client.js
+++ client.js
@@ -3,29 +3,48 @@ import {uid} from "uid/secure";
 // import {VERSION} from "./constants.js";
 
 // Global store
-const store = idb.createStore("kodi", "kodi-notebooks");
+const notebooksStore = idb.createStore("kodi", "kodi-notebooks");
 
 // Client object
 const client = {
     config: {},
     session: null,
-    getNotebooks: () => {
-        return Promise.resolve({});
+    listNotebooks: async () => {
+        const notebooks = [];
+        await notebooksStore("readonly", store => {
+            store.openCursor().addEventListener("success", event => {
+                const cursor = event.target.result;
+                if (cursor) {
+                    notebooks.push({
+                        id: cursor.key,
+                        title: cursor.value?.title || "untitled",
+                        description: cursor.value?.description || "Description of this notebook.",
+                        private: true,
+                        updatedAt: cursor.value?.updatedAt,
+                    });
+                    return cursor.continue();
+                }
+            });
+            return idb.promisifyRequest(store.transaction);
+        });
+        return notebooks.sort((a, b) => {
+            return b.updatedAt - a.updatedAt;
+        });
     },
     addNotebook: data => {
         const id = uid(16);
-        return idb.set(id, data, store).then(() => {
+        return idb.set(id, data, notebooksStore).then(() => {
             return id;
         });
     },
     getNotebook: id => {
-        return idb.get(id, store);
+        return idb.get(id, notebooksStore);
     },
     updateNotebook: (id, newData) => {
-        return idb.update(id, prevData => ({...prevData, ...newData}), store);
+        return idb.update(id, prevData => ({...prevData, ...newData}), notebooksStore);
     },
     deleteNotebook: id => {
-        return idb.del(id, store);
+        return idb.del(id, notebooksStore);
     },
 };
 
