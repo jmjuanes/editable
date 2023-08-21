6f335f94e891f318fe1183e0480d0f93ef9947ed
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-08-21 11:04:10 +0000

    Improve profile page for allowing deleting notebooks

--- components/App.jsx
+++ components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Rouct from "rouct";
 import {createNotebook} from "../notebook.js";
 import {ClientProvider, useClient} from "../contexts/ClientContext.jsx";
+import {useForceUpdate} from "../hooks/useForceUpdate.js";
 import {Layout} from "./Layout.jsx";
 
 import IndexPage from "../pages/index.mdx";
@@ -9,6 +10,7 @@ import ProfilePage from "../pages/profile.mdx";
 import NotebookPage from "../pages/n.mdx";
 
 const InnerApp = () => {
+    const [updateKey, forceUpdate] = useForceUpdate();
     const client = useClient();
     const handleCreateNotebook = () => {
         const newNotebook = createNotebook();
@@ -27,7 +29,16 @@ const InnerApp = () => {
                     )} />
                     <Rouct.Route exact path="/profile" render={() => (
                         <ProfilePage
+                            key={updateKey}
                             onCreateNotebook={handleCreateNotebook}
+                            onOpenNotebook={notebookId => {
+                                Rouct.redirect(`/n?id=${notebookId}`);
+                            }}
+                            onDeleteNotebook={notebookId => {
+                                client.deleteNotebook(notebookId).then(() => {
+                                    forceUpdate();
+                                });
+                            }}
                         />
                     )} />
                     <Rouct.Route exact path="/n" render={request => (
