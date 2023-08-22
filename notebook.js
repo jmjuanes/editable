46b93726bcde3bd6630a6a9d3631aa90f43ae180
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-08-22 11:09:40 +0000

    Added notebook export function

--- notebook.js
+++ notebook.js
@@ -8,6 +8,14 @@ import {VERSION, CDN_URL, CELL_TYPES, CONSOLE_LEVELS} from "./constants.js";
 
 const AsyncFunction = Object.getPrototypeOf(async function (){}).constructor;
 
+// Importing sections
+const IMPORT_SECTIONS = {
+    NONE: "",
+    FRONTMATTER: "frontmatter",
+    TEXT_CELL: "text_cell",
+    CODE_CELL: "code_cell",
+};
+
 // Create a new notebook context
 export const createNotebookContext = () => {
     return {
@@ -37,8 +45,8 @@ export const createNotebook = () => {
     return {
         version: VERSION,
         title: "untitled",
-        description: "",
         isFork: false,
+        locked: false,
         cells: [
             // createNotebookCell(CELL_TYPES.TEXT, ""),
             createNotebookCell(CELL_TYPES.CODE, `return "Hello world!";`),
@@ -50,12 +58,69 @@ export const createNotebook = () => {
 
 // Export notebook as Markdown file
 export const exportNotebook = notebook => {
-    return null;
+    const endl = "\n";
+    return new Promise(resolve => {
+        const output = [
+            "---",
+            `title: '${notebook.title}'`,
+            `createdAt: ${notebook.createdAt}`,
+            `updatedAt: ${notebook.updatedAt}`,
+            "---",
+            "",
+            ...notebook.cells.map(cell => {
+                if (cell.type === CELL_TYPES.CODE) {
+                    const codeBlock = [
+                        "```{javascript editable=true}",
+                        cell.value,
+                        "```",
+                        "",
+                    ];
+                    return codeBlock.join(endl);
+                }
+                // Other case, just return cell value
+                return cell.value;
+            }),
+        ];
+        return resolve(output.join(endl));
+    });
 };
 
 // Import notebook from markdown file
 export const importNotebook = data => {
-    return null;
+    // const endl = "\n";
+    // return new Promise(resolve => {
+    //     const notebook = {
+    //         ...createNotebook(),
+    //         cells: [],
+    //         locked: true,
+    //     };
+    //     let lastCell = null;
+    //     let reading = IMPORT_SECTIONS.NONE;
+    //     data.split(endl).forEach(line => {
+    //         if (line === "---" && !reading) {
+    //             reading = IMPORT_SECTIONS.FRONTMATTER;
+    //         }
+    //         else if (line === "---" && reading === IMPORT_SECTIONS.FRONTMATTER) {
+    //             reading = IMPORT_SECTIONS.NONE;
+    //         }
+    //         // Check if we are reading the frontmatter value
+    //         else if (reading === IMPORT_SECTIONS.FRONTMATTER) {
+    //             const [name, value] = line.split(": ");
+    //             notebook[name] = JSON.parse(value);
+    //         }
+    //         // Check if we are reading a code block
+    //         else if (line === "```{javascript editable=true}") {
+    //             if (reading === IMPORT_SECTIONS.TEXT_CELL && lastCell && lastCell.length > 0) {
+    //                 notebook.cells.push(createNotebookCell(CELL_TYPES.TEXT, lastCell.join(endl)));
+    //             }
+    //             lastCell = [];
+    //             reading = IMPORT_SECTIONS.CODE_CELL;
+    //         }
+    //         else if (line === "```" && reading === IMPORT_SECTIONS.CODE_CELL)
+    //     });
+
+    //     return resolve(notebook);
+    // });
 };
 
 // Create function code
