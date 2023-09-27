7f185877a2b1a78ae52048f926d0a1ff747615d7
Josemi <josemijuanes@gmail.com>
2023-09-27 20:28:34 +0200

    Display cell ID in cell header

--- components/CellHeader.jsx
+++ components/CellHeader.jsx
@@ -12,20 +12,30 @@ const Separator = () => (
 );
 
 export const CellHeader = props => (
-    <div className="w-full flex justify-end items-center gap-2 py-2">
-        {props.showDeleteButton && (
-            <React.Fragment>
-                <Separator />
-                <Button
-                    icon={(<TrashIcon />)}
-                    onClick={props.onDelete}
-                />
-            </React.Fragment>
-        )}
+    <div className="w-full flex justify-between items-center gap-2 py-2">
+        <div className="flex items-center gap-2">
+            {props.id && (
+                <div className="text-xs text-gray-400 hover:text-gray-600 cursor-pointer">
+                    <strong>cell:{props.id}</strong>
+                </div>
+            )}
+        </div>
+        <div className="flex items-center gap-2">
+            {props.showDeleteButton && (
+                <React.Fragment>
+                    <Separator />
+                    <Button
+                        icon={(<TrashIcon />)}
+                        onClick={props.onDelete}
+                    />
+                </React.Fragment>
+            )}
+        </div>
     </div>
 );
 
 CellHeader.defaultProps = {
+    id: null,
     showDeleteButton: false,
     onDelete: null,
     onInsert: null,
