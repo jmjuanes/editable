7e28747e9136f03b78ad4d7b9be882601c2d1730
Josemi <josemijuanes@gmail.com>
2023-09-28 20:24:05 +0200

    Add utility to copy text to clipboard

--- utils.js
+++ utils.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+// Delay the execution of the provided function
+export const delay = (ms, callback) => window.setTimeout(callback, ms);
+
 // Stop event propagation
 export const stopEventPropagation = event => {
     event.stopPropagation();
@@ -13,4 +16,13 @@ export const isDOMElement = element => {
 // Check if the provided value is a React Element
 export const isReactElement = element => {
     return !!element && React.isValidElement(element);
+};
+
+// Copy text to clipboard
+export const copyTextToClipboard = text => {
+    if (navigator?.clipboard) {
+        return navigator.clipboard.writeText(text);
+    }
+    // TODO: use an alternate method to copy to clipboard
+    return Promise.reject(null);
 };
