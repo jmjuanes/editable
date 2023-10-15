155afbd4eb3eef0e1d3a68a56551eb545297ef66
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-10-15 16:55:40 +0000

    Working in console and errors badges

--- components/Result.jsx
+++ components/Result.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import classNames from "classnames";
-import {renderIcon, XCircleIcon} from "@josemi-icons/react";
+import {renderIcon, ChevronDownIcon, ChevronRightIcon} from "@josemi-icons/react";
 import {CONSOLE_LEVELS} from "../constants.js";
 import {Value} from "./Value.jsx";
 import {isDOMElement, isReactElement} from "../utils.js";
@@ -9,35 +9,42 @@ import {isDOMElement, isReactElement} from "../utils.js";
 // Console levels info
 const consoleLevels = {
     [CONSOLE_LEVELS.LOG]: {
-        className: "bg-blue-400",
+        className: "bg-gray-200 text-gray-900",
         icon: "info-circle",
     },
     [CONSOLE_LEVELS.INFO]: {
-        className: "bg-teal-400",
+        className: "bg-blue-200 text-gray-900",
         icon: "info-circle",
     },
     [CONSOLE_LEVELS.WARNING]: {
-        className: "bg-yellow-500",
+        className: "bg-yellow-200 text-gray-900",
         icon: "exclamation-triangle",
     },
     [CONSOLE_LEVELS.ERROR]: {
-        className: "bg-red-400",
+        className: "bg-red-200 text-gray-900",
         icon: "exclamation-circle",
     },
 };
+
+// Render a pill icons
+const PillIcon = props => (
+    <div className={`${props.className} flex items-start rounded-md py-2 px-1`}>
+        <div className="flex text-lg">
+            {renderIcon(props.icon)}
+        </div>
+    </div>
+);
 
 // Print logs messages
 const Logs = props => {
     return props.items.map((item, index) => {
         const {className, icon} = consoleLevels[item.level];
         return (
-            <div key={`log-${index}`} className="flex items-center flex-nowrap">
-                <div className="w-12 flex justify-end">
-                    <div className={`${className} flex text-white rounded-md p-1 mr-2`}>
-                        <div className="flex">{renderIcon(icon)}</div>
-                    </div>
+            <div key={`log-${index}`} className="flex flex-nowrap">
+                <div className="w-12 flex justify-end items-start">
+                    <PillIcon className={className} icon={icon} />
                 </div>
-                <div className="editable-console p-2 rounded-md w-full grow">
+                <div className="editable-console px-3 py-2 rounded-md w-full grow">
                     <div className="text-sm">{item.message}</div>
                 </div>
             </div>
@@ -48,11 +55,16 @@ const Logs = props => {
 export const Result = props => {
     const container = React.useRef(null);
     const root = React.useRef(null);
+    const [compact, setCompact] = React.useState(false);
     const isHtmlValue = isDOMElement(props.value) || isReactElement(props.value);
     const classList = classNames({
         "flex flex-col gap-2": true,
         "o-50": !props.isCurrentValue,
     });
+    // Toggle compact view
+    const toggleCompact = () => {
+        return setCompact(prevCompact => !prevCompact);
+    };
     // Effect for mounting rendered HTML content into container
     React.useEffect(() => {
         if (!props.error && isHtmlValue) {
@@ -77,16 +89,20 @@ export const Result = props => {
             {props.error && (
                 <div className="flex w-full flex-nowrap">
                     <div className="flex justify-end w-12">
-                        <div className="flex items-center bg-red-500 p-1 rounded-md mr-2">
-                            <div className="flex text-white text-lg">
-                                <XCircleIcon />
+                        <div className="flex px-1 py-2 cursor-pointer" onClick={toggleCompact}>
+                            <div className="flex">
+                                {compact ? <ChevronRightIcon /> : <ChevronDownIcon />}
                             </div>
                         </div>
+                        <PillIcon
+                            className="bg-red-200 text-gray-900"
+                            icon="x-circle"
+                        />
                     </div>
-                    <div className="gap-4 p-3 bg-red-100 rounded-md w-full">
-                        <div className="text-sm text-red-900">
-                            <b>{props.errorType}</b>: {props.errorMessage}
-                        </div>
+                    <div className="gap-4 px-3 py-2 bg-red-100 rounded-md w-full">
+                        <pre className="block text-sm text-red-900">
+                            <b>{props.errorType}</b> {compact ? null : props.errorMessage}
+                        </pre>
                     </div>
                 </div>
             )}
