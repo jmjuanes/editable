12df645861dc293cb74609b9e1d235008e01e12b
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-10-22 09:02:12 +0000

    Fix modal colors

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -5,13 +5,13 @@ export const Modal = props => (
     <React.Fragment>
         <div className="fixed w-full h-full bg-white o-90 z-9 top-0 left-0 backdrop-blur-md" />
         <div className="fixed w-full h-full flex items-center justify-center z-10 top-0 left-0">
-            <div className="rounded-lg bg-white w-full p-6 max-w-lg border border-gray-200 shadow-sm">
+            <div className="rounded-lg bg-white w-full p-6 max-w-lg border border-neutral-100 shadow-sm">
                 <div className="flex items-center justify-between mb-4 select-none">
                     <div className="flex items-center text-lg font-bold">
                         <span>{props.title}</span>
                     </div>
                     <div className="flex group" onClick={props.onClose}>
-                        <div className="flex text-2xl text-gray-500 group-hover:text-gray-700 cursor-pointer">
+                        <div className="flex text-2xl text-neutral-500 group-hover:text-neutral-900 cursor-pointer">
                             <CloseIcon />
                         </div>
                     </div>
