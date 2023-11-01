eced33b553255e7925421d890a11345a58ac67cf
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-11-01 16:55:39 +0100

    feat: allow rendering a map from a coordinates object (#16)

--- index.jsx
+++ index.jsx
@@ -9,6 +9,7 @@ import {NotebookProvider} from "./contexts/NotebookContext.jsx";
 import "./style.css";
 import "lowcss/dist/low.css";
 import "codecake/codecake.css";
+import "tyler-js/tyler.css";
 
 createRoot(document.getElementById("root"))
     .render((
